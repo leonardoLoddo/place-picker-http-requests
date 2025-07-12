dc59a9e0e15b420bf94b380fbc5d3919a26a5c75
leonardoLoddo <20loddoloddo@gmail.com>
2025-07-12 10:52:10 +0200

    refactoring fetch con async await

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -4,12 +4,18 @@ import Places from "./Places.jsx";
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:3000/places") //mando la richiesta get '/places' al backend sulla porta 3000
-      .then((response) => response.json()) //riconverto la risposta da json al suo tipo di dato originale
-      .then((data) => {
-        //ora posso utilizzare i dati in questo contesto
-        setAvailablePlaces(data.places); //utilizzo i dati per valorizzare lo state
-      });
+    async function fetchPlaces() {
+      // con await aspetto che la richiesta HTTP al backend venga completata (cioè che fetch risponda)
+      const response = await fetch("http://localhost:3000/places"); //mando la richiesta get '/places' al backend sulla porta 3000
+
+      // con await aspetto che il contenuto della risposta venga trasformato in oggetto JavaScript.
+      const data = await response.json(); //riconverto la risposta da json al suo tipo di dato originale
+
+      //ora posso utilizzare i dati in questo contesto
+      setAvailablePlaces(data.places); //utilizzo i dati per valorizzare lo state
+    }
+
+    fetchPlaces();
   }, []); //utilizzo useEffect in modo tale da non creare un loop infinito andando a rieseguire ogni volta la fetch, che a sua volta settando lo state fará rieseguire la funzione componente
 
   return (
